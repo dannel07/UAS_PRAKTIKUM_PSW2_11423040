a69c72d6fa946bb8de862ff0d9992553c3c39972
Danielsinambela707 <145238890+Danielsinambela707@users.noreply.github.com>
2024-06-07 09:41:57 +0700

    commit1
    y

---
+++ resources/views/components/application-mark.blade.php
@@ -0,0 +1,4 @@
+<div class="inline-flex items-center">
+  <img src="{{ asset('images/logo hasea.png') }}" alt="" class="h-16 w-16">
+  <h1 class="text-3xl font-bold text-blue-500">DEL Badminton </h1>
+</div>
