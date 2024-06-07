486601bc5a4e34ca8266bf44123db715f0f39ad8
Danielsinambela707 <145238890+Danielsinambela707@users.noreply.github.com>
2024-06-07 12:04:59 +0700

    1
    dsfdsklfsjk

--- resources/views/components/application-mark.blade.php
+++
@@ -1,4 +0,0 @@
-<div class="inline-flex items-center">
-  <img src="{{ asset('images/logo hasea.png') }}" alt="" class="h-16 w-16">
-  <h1 class="text-3xl font-bold text-blue-500">DEL Badminton </h1>
-</div>
--- resources/views/components/checkbox.blade.php
+++
@@ -1,1 +0,0 @@
-<input type="checkbox" {!! $attributes->merge(['class' => 'rounded border-gray-300 text-blue-600 shadow-sm focus:ring-blue-500']) !!}>
